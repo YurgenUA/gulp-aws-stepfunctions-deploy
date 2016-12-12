a763dd1a403751800b6393797ff18c0b4f4c82e5
YurgenUA <y.fenyuk@levi9.com>
2016-12-12 15:27:45 +0200

    1st version

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
 # gulp-aws-stepfunctions-deploy
-A gulp task to deploy AWS Step Functions.
+__Version 0.0.1__
+
+This package uses the [aws-sdk (node)](http://aws.amazon.com/sdk-for-node-js/).
+
+[NPM](https://www.npmjs.com/package/gulp-aws-stepfunctions-deploy)
+
+## Install
+
+    npm install gulp-aws-stepfunctions-deploy
+
+## Usage
+
+### Including + Setting Up Config
+
+```js
+    var gulp = require('gulp');
+    var aws-sf = require('gulp-aws-stepfunctions-deploy')(config);
+```
+
+...where config is something like...
+
+```js
+var config = {
+    accessKeyId: "YOURACCESSKEY",
+    secretAccessKey: "YOUACCESSSECRET"
+}
+
+//  ...or...
+
+var config = JSON.parse(fs.readFileSync('private/awsaccess.json'));
+
+//  ...or to use IAM settings...
+
+var config = { useIAM: true };
+
+// ...or to use IAM ...
+
+var awssf = require('gulp-aws-stepfunctions-deploy')(
+    {useIAM:true},
+);
+
+// or {} / null
+
+```
+
+The optional `config` argument can include any option available (like `region`) available in the [AWS Config Constructor](http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property). By default all settings are undefined. 
+
+**Per AWS best practices**, the recommended approach for loading credentials is to use the shared credentials file (`~/.aws/credentials`). You can also set the `aws_access_key_id` and `aws_secret_access_key` environment variables or specify values directly in the gulpfile via the `accessKeyId` and `secretAccessKey` options.  
+
+If you want to use an AWS profile in your `~/.aws/credentials` file just set
+the environment variable AWS_PROFILE with your profile name before invoking
+your gulp task:
+
+```sh
+AWS_PROFILE=myprofile gulp upload
+```
+
+If you are using **IAM** settings, just pass the noted config (`{useIAM:true}`) in order to default to using IAM.  More information on using [IAM settings here](https://aws.amazon.com/documentation/iam/). 
+
+Feel free to also include credentials straight into your `gulpfile.js`, though be careful about committing files with secret credentials in your projects!
+
+Having AWS Key/Secrets may not be required by your AWS/IAM settings.  Errors thrown by the request should give your permission errors.
+
+
+### Gulp Task
+
+Create a task.
+
+```js
+gulp.task("upload", function() {
+    gulp.src("./dir/with/step_function/*.json")
+        .pipe(awssf({
+            //ArnRolePrefix //optional.
+            StepFunctionName: 'STRING_VALUE', //reguired 
+            Recreate: true //optional.
+        }))
+    ;
+});
+```
+
+
+## Options
+
+**ArnRolePrefix** *(optional)*
+
+Type: `string`
+
+Set IAM Roled under which function executes. You can either specify desired IAM Role and omit to use pre-created `StatesExecutionRole-${gconfig.region}`
+
+
+**StepFunctionName** *(required)*
+
+Type: `string`
+
+Step Function name.
+
+**Recreate** *(optional)*
+
+Type: `bool`
+
+Default is false. If set, code check (and remove) function with name specified, before create new one.
+
+### gulp-aws-stepfunctions-deploy-plugin options
+
+## AWS-SDK References
+
+* [AWS Config Constructor](http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property)
+
+----------------------------------------------------
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,192 @@
+'use strict'
+/****
+    gulp-aws-stepfunctions-deploy
+    gulp plugin for uploading Step Functions to the AWS
+****/
+
+const es = require('event-stream');
+const gutil = require('gulp-util');
+const AWS = require('aws-sdk');
+const path = require('path');
+const _ = require('underscore');
+const through = require('through2');
+const PluginError = gutil.PluginError;
+
+let gulpPrefixer;
+
+const PLUGIN_NAME = 'gulp-aws-stepfunctions-deploy';
+
+let gconfig;
+
+var _get_iam_role = function (arn_role_prefix) {
+    let arn_role_prefix_copy = arn_role_prefix;
+    return new Promise((resolve, reject) => {
+        var params = {
+            RoleName: arn_role_prefix_copy
+        };
+        let iam = new AWS.IAM();
+        iam.getRole(params, function (err, data) {
+            if (err)
+                reject(err);
+            else
+                resolve(data.Role.Arn);
+        });
+    });
+}
+
+var _remove_state_machine = function (deploy_options) {
+    let deploy_options_copy = deploy_options;
+    let stepfunctions = new AWS.StepFunctions();
+    return new Promise((resolve, reject) => {
+        if (!deploy_options_copy.recreate)
+            return resolve();
+
+        return new Promise((res, rej) => {
+            stepfunctions.listStateMachines({}, function (err, data) {
+                if (err) {
+                    return rej("StepFunctions listStateMachines Error: " + err.stack);
+                }
+                else
+                var existingMachine = _.find(data.stateMachines, it => {return it.name == deploy_options_copy.function_name})
+                if (!existingMachine)
+                    return res();
+                else
+                    return res(existingMachine.stateMachineArn);
+            });
+        })
+        .then((existingMachineArn) => {
+            if (!existingMachineArn){
+                return resolve();
+            }
+            else{
+                stepfunctions.deleteStateMachine({stateMachineArn: existingMachineArn}, function(err, data) {
+                if (err) 
+                    return reject("StepFunctions deleteStateMachine Error: " + err.stack);
+                else
+                    setTimeout(() => {
+                        return resolve();
+                    }, 60000);
+                });
+
+            }
+        })
+
+    });
+}
+
+
+var _deploy_state_machine = function (deploy_options) {
+    let deploy_options_copy = deploy_options;
+
+    return new Promise((resolve, reject) => {
+        var params = {
+            definition: deploy_options_copy.file._contents.toString() ,
+            name: deploy_options.function_name,
+            roleArn: deploy_options_copy.role_arn
+        };
+        let stepfunctions = new AWS.StepFunctions();
+        stepfunctions.createStateMachine(params, function (err, data) {
+            if (err) {
+                return reject("StepFunctions createStateMachine Error: " + err.stack);
+            }
+            else
+                return resolve(data);
+        });
+    });
+}
+
+var _deploy = function (options) {
+
+    let arn_role_prefix = options.ArnRolePrefix || `StatesExecutionRole-${gconfig.region}`;
+    
+    if (!_.has(options, 'Recreate') && !options.Recreate) {
+        options.Recreate = false;
+    }
+    
+
+    let step_function_name = options.StepFunctionName;
+
+    let sf_file;
+
+    return through.obj((file, enc, cb) => {
+
+        if (file.isNull()) {
+            return cb();
+        }
+        if (file.isStream()) {
+            return cb(new gutil.PluginError(PLUGIN_NAME, 'Streaming is not supported'));
+        }
+        if (!sf_file) {
+            sf_file = file;
+        }
+        cb();
+
+    }, cb => {
+        let deploy_options = {file: sf_file, function_name: step_function_name, recreate: options.Recreate};
+
+        return _remove_state_machine(deploy_options)
+            .then(() =>{
+                return _get_iam_role(arn_role_prefix);
+            })
+            .then(arn => {
+                deploy_options.role_arn = arn; 
+                return _deploy_state_machine(deploy_options)
+            }).then((res) => {
+                console.log('State Machine result:' + JSON.stringify(res));
+                return cb(null);
+            })
+
+            .catch(err => {
+                return cb(new gutil.PluginError(PLUGIN_NAME, "Error: " + err));
+            });
+
+    });
+}
+
+
+// ===== EXPORTING MAIN PLUGIN FUNCTION =====
+// `config` now takes the paramters from the AWS-SDK constructor:
+// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+
+module.exports = function (config) {
+    var aws_config = {};
+
+    if (_.isUndefined(config)) {
+        config = {};
+    }
+
+    //  If using IAM
+    if (_.has(config, 'useIAM') && config.useIAM) {
+        config = {};
+    }
+
+    if (!_.has(config, 'region') && !config.region) {
+        config.region = 'eu-west-1';
+    }
+
+
+    //  Intentionally not mandating the accessKeyId and secretAccessKey as they
+    //  will be loaded automatically by the SDK from either environment variables
+    //  or the credentials file.
+    //  http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html
+
+    // Configure the proxy if an environment variable is present.
+
+    if (process.env.HTTPS_PROXY) {
+        gutil.log("Setting https proxy to %s", process.env.HTTPS_PROXY);
+
+        if (!aws_config.httpOptions) {
+            aws_config.httpOptions = {};
+        }
+
+        var HttpsProxyAgent = require('https-proxy-agent');
+
+        aws_config.httpOptions.agent = new HttpsProxyAgent(process.env.HTTPS_PROXY);
+    }
+
+    //  Update the global AWS config if we have any overrides
+    AWS.config.update(_.extend({}, config, aws_config));
+
+    gconfig = config;
+    return _deploy;
+};
